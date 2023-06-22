9fa3435350b29bb624c4e26f4a4d518f7855fbc8
Choi-jujuyeon <chlwndus1216@naver.com>
2023-06-22 10:18:14 +0900

    Box-컴포넌트 생성

--- my-app/README.md
+++ my-app/README.md
@@ -1,3 +1,7 @@
 # 강의노트
 
 👉 FC ( Function Component )
+
+👉 Props 사용법
+
+    step 01. components/Box.jsx 생성하기
---
+++ my-app/src/components/Box.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Box: FC = () => {
+    return <div></div>;
+};
+
+export default Box;
